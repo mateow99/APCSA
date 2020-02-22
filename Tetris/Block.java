bcf3361f5b3fc8c517769c043d08f587ad8a94d5
mateow99 <61336154+mateow99@users.noreply.github.com>
2020-02-22 11:29:00 -0800

    Add files via upload

---
+++ Tetris/Block.java
@@ -0,0 +1,130 @@
+import java.awt.Color;
+/**
+ * Block class maintains information about a block.
+ * This class is extremely flexible to multiple games;
+ * such as Tile Game, Tetris, and Tic-Tac-Toe.
+ * 
+ * @author  Dave Feinberg
+ * @author  Richard Page
+ * @author  Susan King
+ * @author  Mateo Weiner
+ * @version May 13, 2015
+ */
+public class Block
+{
+    private MyBoundedGrid<Block> grid;
+    private Location location;
+    private Color color;
+    private String text;
+
+    /**
+     * Constructs a blue block.
+     */
+    public Block()
+    {
+        color = Color.BLUE;
+        grid = null;
+        location = null;
+    }
+
+    /**
+     * Gets the color of this block.
+     * 
+     * @return the color of this block
+     */
+    public Color getColor()
+    {
+        return color;
+    }
+
+    /**
+     * Sets the color of this block to newColor.
+     * 
+     * @param newColor  the new color of this block
+     */
+    public void setColor(Color newColor)
+    {
+        color = newColor;
+    }
+
+    /**
+     * Gets the grid of this block, or null if this block is not contained
+     * in a grid.
+     * 
+     * @return the grid
+     */
+    public MyBoundedGrid<Block> getGrid()
+    {
+        return grid;
+    }
+
+    /**
+     * Gets the location of this block, or null if this block is not contained
+     * in a grid.
+     * 
+     * @return this block's location, or null if this block is not in the grid
+     */
+    public Location getLocation()
+    {
+        return location;
+    }
+
+    /**
+     * Removes this block from its grid.
+     *
+     * @precondition  this block is contained in a grid
+     */
+    public void removeSelfFromGrid()
+    {
+        grid.remove(getLocation());
+        grid = null;
+        location = null;
+    }
+
+    /**
+     * Puts this block into location loc of grid gr.
+     * If there is another block at loc, it is removed.
+     * 
+     * @precondition  (1) this block is not contained in a grid
+     *                (2) loc is valid in gr
+     *               
+     * @param gr  the grid to place this block
+     * @param loc the location to place this block
+     */
+    public void putSelfInGrid(MyBoundedGrid<Block> gr, Location loc)
+    {
+        grid = gr;
+        if (grid.get(loc) != null)
+        {
+            grid.get(loc).removeSelfFromGrid();
+        }
+        grid.put(loc, this);
+        location = loc;
+    }
+
+    /**
+     * Moves this block to newLocation.
+     * If there is another block at newLocation, it is removed.
+     *
+     * @precondition  (1) this block is contained in a grid
+     *                (2) newLocation is valid in the grid of this block
+     *                
+     * @param newLocation  the location that the block is to be moved
+     */
+    public void moveTo(Location newLocation)
+    {
+        MyBoundedGrid<Block> oldGrid = getGrid();
+        removeSelfFromGrid();
+        putSelfInGrid(oldGrid, newLocation);
+    }
+
+    /**
+     * Returns a string with the location and color of this block.
+     * 
+     * @return location and color information about the block
+     */
+    public String toString()
+    {
+        return "Block[location=" + location + ",color=" + color + "]";
+    }
+}
---
+++ Tetris/Tetrad.java
@@ -0,0 +1,224 @@
+import java.awt.Color;
+/**
+ * The shapes for the Tetris game.
+ * 
+ * @author Mateo Weiner
+ * @version 1/26/19
+ */
+public class Tetrad
+{
+    private Block[] blocks;
+    private MyBoundedGrid<Block> grid;
+    private Location[] locs;
+    private int random;
+
+    /**
+     * Constructs one shape using four blocks
+     * 
+     * @param gr the grid that is used for the tetrads
+     */
+    public Tetrad(MyBoundedGrid<Block> gr)
+    {
+        blocks = new Block[4];
+        for (int i = 0; i < 4; i++)
+        {
+            blocks[i] = new Block();
+        }
+        grid = gr;
+        random = (int)(Math.random()*7);
+        locs = new Location[4];
+
+        if (random == 0)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                blocks[x].setColor(Color.RED);
+            }
+            locs[0] = new Location(1, 4);
+            locs[1] = new Location(2, 4);
+            locs[2] = new Location(3, 4);
+            locs[3] = new Location(0, 4);
+        }
+        if (random == 1)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                blocks[x].setColor(Color.GRAY);
+            }
+            locs[0] = new Location(0, 4);
+            locs[1] = new Location(0, 3);
+            locs[2] = new Location(1, 4);
+            locs[3] = new Location(0, 5);
+        }
+        if (random == 2)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                blocks[x].setColor(Color.CYAN);
+            }
+            locs[0] = new Location(0, 4);
+            locs[1] = new Location(0, 5);
+            locs[2] = new Location(1, 4);
+            locs[3] = new Location(1, 5);
+        }
+        if (random == 3)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                blocks[x].setColor(Color.YELLOW);
+            }
+            locs[0] = new Location(2, 4);
+            locs[1] = new Location(1, 4);
+            locs[2] = new Location(0, 4);
+            locs[3] = new Location(2, 5);
+        }
+        if (random == 4)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                blocks[x].setColor(Color.MAGENTA);
+            }
+            locs[0] = new Location(2, 5);
+            locs[1] = new Location(1, 5);
+            locs[2] = new Location(0, 5);
+            locs[3] = new Location(2, 4);
+        }
+        if (random == 5)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                blocks[x].setColor(Color.BLUE);
+            }
+            locs[0] = new Location(1, 4);
+            locs[1] = new Location(0, 4);
+            locs[2] = new Location(1, 3);
+            locs[3] = new Location(0, 5);
+        }
+        if (random == 6)
+        {
+            for (int x = 0; x < 4; x++)
+            {
+                blocks[x].setColor(Color.GREEN);
+            }
+            locs[0] = new Location(0, 4);
+            locs[1] = new Location(0, 3); 
+            locs[2] = new Location(1, 4);
+            locs[3] = new Location(1, 5);
+        }
+        addToLocations(grid, locs);
+    }
+
+    /**
+     * adds a block to the grid
+     * 
+     * @param  gr   the grid which will contain the block
+     * @param  locations   the array which will contain the blocks
+     * 
+     * @precondition the tetrad is not in the grid
+     */
+    public void addToLocations(MyBoundedGrid<Block> gr, Location[] locations)
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            blocks[x].putSelfInGrid(gr, locations[x]);
+        }
+        locs = locations;
+    }
+
+    /**
+     * removes a block
+     * 
+     * @precondition the block being removes is currently in the grid
+     * 
+     * @return the array of locations
+     * 
+     */
+    private Location[] removeBlocks()
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            blocks[x].removeSelfFromGrid();
+        }
+        return locs;
+    }
+
+    /**
+     * checks locations on a grid to see if they are availible
+     * 
+     * @return true if the locations at the specified grid are empty; otherwise,
+     *         false
+     * @param gr         the grid to look for the locations in
+     * @param locations  the locations to check for availibility
+     */
+    private boolean areEmpty(MyBoundedGrid<Block> gr, Location[] locations)
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            if (!gr.isValid(locations[x]) || gr.get(locations[x]) != null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * moves the tetrad to a new location
+     * 
+     * @return true if the locations at the specified grid are empty; otherwise,
+     *         false
+     * @param deltaRow  the number of spaces to move the tetrad down
+     * @param deltaCol the number of spaces to move the tetrad to the right
+     */
+    public boolean translate(int deltaRow, int deltaCol)
+    {
+        Location[] oldLocs = removeBlocks();
+        Location[] newLocs = new Location[4];
+        for (int x = 0; x < 4; x++)
+        {
+            newLocs[x] = new Location(locs[x].getRow() + deltaRow, locs[x].getCol() + deltaCol);
+        }
+        if (areEmpty(grid, newLocs))
+        {
+            addToLocations(grid, newLocs);
+            return true;
+        }
+        else
+        {
+            addToLocations(grid, oldLocs);
+            return false;
+        }
+    }
+
+    /**
+     * rotates the tetrad
+     * 
+     * @return true if the rotate is successful; otherwise,
+     *         false
+     */
+    public boolean rotate()
+    {
+        Location[] oldLocs = removeBlocks();
+        Location[] newLocs = new Location[4];
+        for (int x = 0; x < 4; x++)
+        {
+            if (random == 2)
+            {
+                addToLocations(grid, oldLocs);
+                return true;
+            }
+            newLocs[x] = new Location(locs[0].getRow() - locs[0].getCol()
+                + locs[x].getCol(), locs[0].getCol() + locs[0].getRow() - locs[x].getRow());
+        }
+        if (areEmpty(grid, newLocs))
+        {
+            addToLocations(grid, newLocs);
+            return true;
+        }
+        else
+        {
+            addToLocations(grid, oldLocs);
+            return false;
+        }
+    }
+}
